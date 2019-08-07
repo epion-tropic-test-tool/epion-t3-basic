8f969bcc62a8705ca0f6df7c210891d1bd530d20
Nozomu Takashima <nozomu.takashima@gmail.com>
2019-08-07 23:24:48 +0900

    カスタムFlowをBasicへ移動

---
+++ src/main/java/com/epion_t3/basic/flow/model/BranchFlow.java
@@ -0,0 +1,24 @@
+package com.epion_t3.basic.flow.model;
+
+import com.epion_t3.core.common.annotation.FlowDefinition;
+import com.epion_t3.core.common.bean.scenario.Flow;
+import com.epion_t3.core.flow.runner.impl.BranchFlowRunner;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.bval.constraints.NotEmpty;
+
+@Getter
+@Setter
+@FlowDefinition(id = "Branch", runner = BranchFlowRunner.class)
+public class BranchFlow extends Flow {
+
+    @NotEmpty
+    private String condition;
+
+    @NotEmpty
+    private String trueRef;
+
+    @NotEmpty
+    private String falseRef;
+
+}
