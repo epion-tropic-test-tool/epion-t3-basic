1f145fcf0736b31b9b3470c54ed8820c8843eff3
Nozomu Takashima <nozomu.takashima@gmail.com>
2020-12-29 18:47:36 +0900

    add test case.

---
+++ src/test/java/com/epion_t3/basic/command/runner/AddDateRunnerTest.java
@@ -0,0 +1,38 @@
+/* Copyright (c) 2017-2020 Nozomu Takashima. */
+package com.epion_t3.basic.command.runner;
+
+import org.apache.commons.lang3.time.FastDateFormat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+/**
+ * {@link AddDateRunner} のテストケース.
+ */
+public class AddDateRunnerTest extends CommandTestBase {
+
+    /**
+     * フォーマッタ.
+     */
+    private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss");
+    /**
+     * フォーマッタ.
+     */
+    private static final FastDateFormat FDF = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss");
+
+    @Test
+    public void test_normal_001() {
+        var targetDate = LocalDateTime.of(2020, 01, 01, 12, 34, 56, 0);
+        var zdt = targetDate.atZone(ZoneId.systemDefault());
+        executeContext.getGlobalVariables().put("target_date", new Date(zdt.toInstant().toEpochMilli()));
+        scenarioRunner.execute(context, executeContext);
+        var expected = targetDate.plus(1, ChronoUnit.DAYS);
+        var actual = (Date) executeContext.getScenarios().get(0).getScenarioVariables().get("added_date");
+        Assertions.assertEquals(DTF.format(expected), FDF.format(actual));
+    }
+}
---
+++ src/test/java/com/epion_t3/basic/command/runner/CommandTestBase.java
@@ -0,0 +1,48 @@
+package com.epion_t3.basic.command.runner;
+
+import com.epion_t3.core.common.context.Context;
+import com.epion_t3.core.common.context.ExecuteContext;
+import com.epion_t3.core.custom.parser.impl.CustomParserImpl;
+import com.epion_t3.core.scenario.parser.impl.ScenarioParserImpl;
+import com.epion_t3.core.scenario.runner.ScenarioRunner;
+import com.epion_t3.core.scenario.runner.impl.ScenarioRunnerImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.nio.file.Path;
+
+public class CommandTestBase {
+
+    /** コンテキスト. */
+    Context context;
+
+    /** 実行コンテキスト */
+    ExecuteContext executeContext;
+
+    /** シナリオ実行処理. */
+    ScenarioRunner scenarioRunner;
+
+    /**
+     * テストケース毎の初期処理を行います.
+     * @param testInfo テスト情報
+     * @param tempDir 仮ディレクトリ
+     */
+    @BeforeEach
+    void beforeEachTest(TestInfo testInfo, @TempDir Path tempDir) {
+        context = new Context();
+        context.getOption().setRootPath(System.getProperty("user.dir") + File.separator + "src/test/resources/CommandTestBase/"+ testInfo.getTestClass().get().getSimpleName() + "/scenario");
+        context.getOption().setTarget(testInfo.getTestMethod().get().getName());
+        context.getOption().setResultRootPath(tempDir.toAbsolutePath().toString());
+        context.getOption().setNoreport(true);
+        executeContext = new ExecuteContext();
+        executeContext.setResultRootPath(tempDir);
+        CustomParserImpl.getInstance().parse(context, executeContext);
+        // シナリオの解析（パース処理）
+        ScenarioParserImpl.getInstance().parse(context, executeContext);
+        // シナリオ実行処理の生成
+        scenarioRunner = new ScenarioRunnerImpl();
+    }
+
+}
